8d148d30cf92b425469be5ccd59e71ab284fc703
奔跑的大肉包 <75438356@qq.com>
2017-12-08 09:04:57 +0800

    commit -m "新闻列表完成"

--- src/components/news/newlist.vue
+++ src/components/news/newlist.vue
@@ -1,27 +1,51 @@
 <template>
   <div id="top">
-      11111
+        <ul class="mui-table-view">
+			<li v-for="(list,index) in lists" :key="index" class="mui-table-view-cell mui-media">
+				<a href="javascript:;">
+					<img class="mui-media-object mui-pull-left" :src="list.img_url">
+					<div class="mui-media-body">
+                        {{list.title}}
+					    <p class='mui-ellipsis' v-text="list.zhaiyao"></p>
+					</div>
+                    <div>
+                        <span class="time">发表时间:{{list.add_time | datafmt}}</span>
+                        <span class="number">点击次数{{list.click}}</span>
+                    </div>
+				</a>
+			</li>
+		</ul>
   </div>
 </template>
 
 <script>
-    export default{
-        data(){
-            return {
-                lists:[]
-            }
-        },
-        created(){
-            this.news();
-        },
-        methods:{
-            news(){
-
-            }
-        }
+export default {
+  data() {
+    return {
+      lists: []
+    };
+  },
+  created() {
+    this.news();
+  },
+  methods: {
+    news() {
+      this.$http.get("http://vue.studyit.io/api/getnewslist").then(res => {
+        this.lists = res.body.message;
+      });
     }
+  }
+};
 </script>
 
 <style scoped>
-
+.mui-table-view .mui-media-object {
+  line-height: 30px;
+  max-width: 60px;
+  height: 60px;
+}
+.time,
+.number {
+  font-size: 12px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -5,29 +5,33 @@ Vue.use(VueRouter);
 import Mint from 'mint-ui';
 Vue.use(Mint);
 import "mint-ui/lib/style.css";
-import "../statics/mui/css/mui.css"
-import "../statics/css/site.css"
+import "../statics/mui/css/mui.css";
+import "../statics/css/site.css";
 import Home from "./components/Home.vue";
 import car from "./components/shopcar/car.vue";
-import newlist from "./components/news/newlist.vue"
+import newlist from "./components/news/newlist.vue";
 import vueResource from "vue-resource";
+import moment from "moment";
 Vue.use(vueResource);
 var router = new VueRouter({
     linkActiveClass: "mui-active",
     routes: [{
-            path: "/home",
-            component: Home
-        },
-        {
-            path: "/shopcar",
-            component: car
-        },
-        {
-            path: "/news/newlist",
-            component: newlist
-        }
+        path: "/home",
+        component: Home
+    },
+    {
+        path: "/shopcar",
+        component: car
+    },
+    {
+        path: "/news/newlist",
+        component: newlist
+    }
     ]
 });
+Vue.filter("datafmt", function (input) {
+    return moment(input).format('YYYY-MM-DD HH:mm:ss')
+})
 new Vue({
     el: "#app",
     router,
